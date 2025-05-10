a147db8defb7ee5db3cfefdae6d39e119e27b045
ZubeyirOflaz <zubeyir.oflaz@gmail.com>
2025-05-10 22:59:22 +0200

    Script renaming and minor changes

--- config.py
+++ config.py
@@ -2,8 +2,12 @@ from typing import NamedTuple
 
 class Config(NamedTuple):
 
-    apply_default_transformation = True
-    custom_transformation_proj4 = None
+    input_resolution = 16 # The resolution of the image data to be converted to STL. The resolution can be 4, 16, 64, 128, 256, or 512 pixels/degree.
+                          # Please note that the higher resolutions create minor artifacts in the output STL file (cause to be investigated).
+
+    apply_default_transformation = True # Whether to apply the default transformation to the image data. Default transformation creates an orthographic projection that centers 
+                                        # the image in Sea of Tranquility in latitude and lunar equator in longitude. 
+    custom_transformation_proj4 = None # Custom proj4 string for the transformation. The apply_default_transformation parameter should be set to False for this to take effect.
     
 
     simplify_mesh : bool = True
@@ -13,6 +17,5 @@ class Config(NamedTuple):
     
     # Phsical properties of the output STL file(s)
     num_stl_divisions : int = 5 # Number of divisions in each axis for the STL file. Creates nXn divisions.
-    total_diameter : int = 1000 # Total diameter of the STL file in millimeters.
-    height : int = 100 # Height range of the STL file in millimeters.
-    zoom_factor : float = 0.5
+    height_range : int = 60 # Height range of the STL file in millimeters.
+    zoom_factor : float = 1.0 # Zoom factor for the image data
